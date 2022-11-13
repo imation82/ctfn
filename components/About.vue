9485f5242e8c5040d1504f1d0ff019ca8a9a57ea
imation82 <odesk.rahman@gmail.com>
2022-11-14 00:00:07 +0600

    About section updated

--- components/About.vue
+++ components/About.vue
@@ -1,34 +1,36 @@
 <template>
     <section id="about" class="about scroll-mt">
-        <div class="visual-top">
-
+        <div class="visual-top-right">
+            <b-img src="../assets/images/top-right.png" alt="Top right"></b-img>
         </div>
-        <div class="visual-bottom">
-
+        <div class="visual-bottom-left">
+            <b-img src="../assets/images/bottom-left.png" alt="Bottom left"></b-img>
         </div>
-        <b-container>
-            <b-row>
-                <b-col md="3">
-                    <b-img class="bounce d-none d-md-block" src="../assets/images/left.jpg" alt="Advertisement" fluid></b-img>
-                </b-col>
-                <b-col md="9">
-                    <header>
-                        <h1>About Us</h1>
-                        <p class="product-header-description">Founded by a team of former investment managers, and
-                            supported by experienced financial journalists, CTFN understands the questions on the minds
-                            of investors and professionals who are impacted by critical activity in mergers and special
-                            situations.</p>
-                        <p class="product-header-description">Our market-leading reporting and our proprietary data
-                            tools seek to address the questions and the information needs of investors and advisors
-                            following the nuanced developments in mergers, dynamic industry sectors, and key special
-                            situations.</p>
-                        <p class="product-header-description mb-5">CTFN provides time-sensitive updates for better
-                            decisions.</p>
-                            <b-btn class="submitBtn" @click="showTrial = !showTrial">Request a Trial</b-btn>
-                    </header>
-                </b-col>
-            </b-row>
-        </b-container>
+        <div class="about-content">
+            <b-container>
+                <b-row>
+                    <b-col md="3">
+                        <b-img class="bounce d-none d-md-block" src="../assets/images/left.png" alt="Advertisement" fluid></b-img>
+                    </b-col>
+                    <b-col md="9">
+                        <header>
+                            <h1>About Us</h1>
+                            <p class="product-header-description">Founded by a team of former investment managers, and
+                                supported by experienced financial journalists, CTFN understands the questions on the minds
+                                of investors and professionals who are impacted by critical activity in mergers and special
+                                situations.</p>
+                            <p class="product-header-description">Our market-leading reporting and our proprietary data
+                                tools seek to address the questions and the information needs of investors and advisors
+                                following the nuanced developments in mergers, dynamic industry sectors, and key special
+                                situations.</p>
+                            <p class="product-header-description mb-5">CTFN provides time-sensitive updates for better
+                                decisions.</p>
+                                <b-btn class="submitBtn" @click="showTrial = !showTrial">Request a Trial</b-btn>
+                        </header>
+                    </b-col>
+                </b-row>
+            </b-container>            
+        </div>
         <div v-if="showTrial" id="trialForm" class="request-trial">
             <b-container>
                 <header class="text-center">
