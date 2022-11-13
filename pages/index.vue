1acb37e89ba22bd34762fa140c9552985fd4589c
imation82 <odesk.rahman@gmail.com>
2022-11-13 23:11:03 +0600

    Section updated

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,6 @@
   <section>
     <HeroSection />
     <About />
-    <TrialForm />
     <Products />
     <Team />
     <Contact />
@@ -16,7 +15,6 @@ export default {
   components: {
     HeroSection: () => import("@/components/HeroSection.vue"),
     About: () => import("@/components/About.vue"),
-    TrialForm: () => import("@/components/TrialForm.vue"),
     Products: () => import("@/components/Products.vue"),
     Team: () => import("@/components/Team.vue"),
     Contact: () => import("@/components/Contact.vue"),
